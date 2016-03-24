e49198aedd67d816ac4c008be73286bb13712255
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-03-24 17:41:25 -0400

    finally got storage working

--- storage.js
+++ storage.js
@@ -1,59 +1,39 @@
-var Sync = require('syncho');
+var _ = require('lodash');
 var logbot = require('./logbot');
+var async = require('async');
 var db = require("redis").createClient(process.env.REDIS_URL);
 
-var storage = {
-	get : function(key){
-		return JSON.parse(db.get.sync(key) || 'null');
-	},
-	set : function(key, val){
-		return db.set.sync(key, JSON.stringify(val))
-	},
-	getAsync : function(key, cb){
-		return db.get(key, function(err, res){
-			if(err || !res) return cb();
-			cb(JSON.parse(res || 'null'));
-		})
-	},
-	setAsync : function(key, val, cb){
-		return db.set(key, JSON.stringify(val), function(err, res){
-			return cb(res);
-		});
-	},
-};
-
-
-//Fallback storage
-var TEMP_STORAGE = {}
-var fallback = {
-	get : function(key){
-		return TEMP_STORAGE[key];
-	},
-	set : function(key, val){
-		TEMP_STORAGE[key] = val;
-	},
-	getAsync : function(key, cb){
-		return cb(TEMP_STORAGE[key]);
-	},
-	setAsync : function(key, val, cb){
-		TEMP_STORAGE[key] = val;
-		return cb();
-	},
-};
-
-
-
-//Check for local fallback
 db.on("error", function(err){
 	if(process.env.PRODUCTION) logbot.warn("Redis Storage Error", "Falling back to temporary storage instance.");
 	db.end();
 	console.log('REDIS ERROR: Falling back to node in-memory storage');
-
-	storage.get = fallback.get;
-	storage.set = fallback.set;
-	storage.getAsync = fallback.getAsync;
-	storage.setAsync = fallback.setAsync;
 });
 
+var STORAGE = {};
 
-module.exports = storage;
+module.exports = storage = {
+	init : function(ready){
+		db.keys('*', function (err, keys) {
+			async.map(keys, function(key, cb){
+				db.get(key, function(err, res){
+					cb(err, [key, JSON.parse(res)]);
+				});
+			}, function(err, res){
+				STORAGE = _.fromPairs(res);
+				ready && ready();
+			})
+		})
+	},
+	get : function(key, cb){
+		if(cb && db.connected){
+			db.get(key, function(err, res){
+				return cb(err, JSON.parse(res))
+			})
+		}
+		return STORAGE[key];
+	},
+	set : function(key, val, cb){
+		STORAGE[key] = val;
+		return db.connected && db.set(key, JSON.stringify(val), cb);
+	},
+};
